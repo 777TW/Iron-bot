4ec60d8e82b9a2d335c0cce4da080b787809aa4a
777TW <104886390+777TW@users.noreply.github.com>
2024-04-28 21:58:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from discord.ext import commands
 import json
 import os
 import asyncio
+dirname = os.path.dirname(__file__)
 
-
-with open(r"C:\Users\happy_\OneDrive\桌面\iron-bot\json\settings.json", 'r', encoding='utf8') as jfile:
+with open(r"{}\json\settings.json".format(dirname), 'r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
 bot = commands.Bot(command_prefix='$', help_command=None, intents=intents, application_id = "1118424692680822835")
@@ -19,7 +19,7 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity = game)
                               
 async def loads():
-    for filename in os.listdir(r"C:\Users\happy_\OneDrive\桌面\iron-bot\categories"):
+    for filename in os.listdir(r"{}\categories".format(dirname):
         if filename.endswith('.py'):
             await bot.load_extension(f'categories.{filename[:-3]}')
 
